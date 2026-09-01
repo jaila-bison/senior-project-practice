c99c4759a09fef55726bb98d837adcef1c7bf925
jaila-bison <168598532+jaila-bison@users.noreply.github.com>
2026-09-01 13:17:48 -0400

    Add developer profile information to app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+i = 'Senior Project Developer Profile'
+n = 'Jaila Walker'
+m = 'Computer Science'
+ti = 'Data Science'
+g = 'Development of a useful project'
 
+print(i, '\n', 'Name', name, '\n', 'Major', major, '\n', 'Interest: ',ti, '\n', 'Goal: ', g)
